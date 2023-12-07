cee1b6dedd7d0420225542facf1f182b04f97df9
Ependi Khadzhiev <khadzhiev909@gmail.com>
2023-12-07 22:15:43 +0300

    Написал контроллер для объявления

---
+++ src/main/java/ru/skypro/homework/controller/AdController.java
@@ -0,0 +1,61 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.model.Ad;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/ads")
+public class AdController {
+    private final AdService adService;
+
+    @GetMapping("getAllAds")
+    public ResponseEntity<List<Ad>> getAllAds() {
+        List<Ad> ads = adService.findAll();
+        if (ads.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ads);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<Ad> getById(@PathVariable Long id) {
+        Ad foundAd = adService.findById(id);
+        if (foundAd == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(foundAd);
+    }
+
+    @PostMapping("addAd")
+    public ResponseEntity<Void> addAd(@RequestBody Ad ad) {
+        if (ad == null) {
+            return ResponseEntity.notFound().build();
+        }
+        adServise.add(ad);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping()
+    public ResponseEntity<Void> updateAd(@RequestBody Ad ad) {
+        Ad foundAd = adService.find(ad);
+        if (foundAd == null) {
+            return ResponseEntity.notFound().build();
+        }
+        Ad correctedAd = adService.updateAd(ad);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteReportById(@PathVariable Long id) {
+        Ad ad = adService.findById(id);
+        if (ad == null) {
+            return ResponseEntity.notFound().build();
+        }
+        adService.delete(ad);
+        return ResponseEntity.ok().build();
+    }
+
+}
